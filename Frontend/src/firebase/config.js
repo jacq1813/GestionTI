8c38ec2b9d51248e4bc61d9d852d29fb8186d071
jacq1813 <l21170454@culiacan.tecnm.mx>
2025-03-13 21:34:09 -0700

    Login

---
+++ Frontend/src/firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlY508tONcV07F1ze36mJI3clWx3VYr14",
+  authDomain: "pw2024-26449.firebaseapp.com",
+  projectId: "pw2024-26449",
+  storageBucket: "pw2024-26449.firebasestorage.app",
+  messagingSenderId: "167107301012",
+  appId: "1:167107301012:web:30a1b14236f5f69f5c6e81"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ Frontend/src/modulos/autenticacion/SignIn.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="contenedor">
+        <form class="formulario">
+            <h2>validacion de usuario</h2>
+            <div class="grupo">
+                <label>Correo:</label>
+                <input type="email" required>
+            </div>
+            <div class="grupo">
+                <label>Contraseña:</label>
+                <input type="password" required>
+            </div>
+            <div class="error">
+                <span>error</span>
+            </div>
+            <button type="submit">
+                Ingresar
+            </button>
+        </form>
+    </div>
+</template>
+
+
+<style scoped>
+.contenedor {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+}
+
+.formulario {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+}
+
+.grupo {
+    display: flex;
+    flex: column;
+    gap: 0.5em;
+}
+
+input {
+    padding: 8px;
+    border: 1px solid #DDD;
+    border-radius: 5px;
+}
+
+button {
+    padding: 10px;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.error {
+    color: red;
+    font-size: 0.9em;
+    margin-top: 0.5em;
+}
+</style>
