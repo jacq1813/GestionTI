1570f23d64efa889c4b900996fbab6509cfd2eda
KingSplatt <malp18774@gmail.com>
2025-03-15 00:53:37 -0700

    Add endpoint to retrieve incidents with detailed information and improve error handling

--- Backend/src/routes/incidentRoutes.ts
+++ Backend/src/routes/incidentRoutes.ts
@@ -9,4 +9,10 @@ router.get('/', async (_req: Request, res: Response) => {
     res.status(200).send(incidents);
 });
 
+//http://localhost:3001/incidents/recandemi
+router.get('/recandemi', async (_req: Request, res: Response) => {
+    const incidents = await incidentService.getIncidentsRecandEmi();
+    res.status(200).send(incidents);
+});
+
 export default router;
--- Backend/src/services/incidentService.ts
+++ Backend/src/services/incidentService.ts
@@ -7,7 +7,22 @@ export const getIncidents = async () => {
     try {
         const [rows] = await connection.query('SELECT *FROM incidencia');
         return rows;
+    } catch (error) {
+        return { error: "no se pudo obtener las incidencias" };
+    }
+}
 
+export const getIncidentsRecandEmi = async () => {
+    try {
+        const [rows] = await connection.query(`SELECT i.Folio,i.Descripcion,i.Fecha,i.Periodo, i.Estado,i.Hora,a.Nombre as 'Aula' , ta.Nombre as 'Tipo Aula', E.Nombre AS  'Emisor', te.Nombre as 'Puesto Emisor', e2.Nombre as 'Receptor',te2.Nombre as 'Puesto Receptor' FROM incidencia i 
+                                                inner join empleado e on i.ID_Emi = e.ID_Emp
+                                                inner join empleado e2 on e2.ID_Emp = i.ID_Rec
+                                                inner join aula a on i.ID_Aula = a.ID_Aul 
+                                                inner join tipoaula ta on ta.ID_TipoAula = a.ID_TipoAula
+                                                inner join tipoempleado te on te.ID_TipEmp = e.ID_TipEmp
+                                                inner join tipoempleado te2 on te2.ID_TipEmp = e2.ID_TipEmp
+                                                ORDER BY Folio`);
+        return rows;
     } catch (error) {
         return { error: "no se pudo obtener las incidencias" };
     }
