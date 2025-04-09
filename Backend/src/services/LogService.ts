4906cdb9f80e6c28e36558d4964d4528514c505f
KingSplatt <malp18774@gmail.com>
2025-04-09 16:46:26 -0700

    Fix database connection port and optimize log queries by removing unnecessary fields

--- Backend/src/services/LogService.ts
+++ Backend/src/services/LogService.ts
@@ -15,13 +15,12 @@ export const getLogs = async () => {
 //obtener todas las bitacoras con detalles
 export const getLogsDetails = async () => {
     try {
-        const [rows] = await connection.query(`SELECT bi.Id_Bitacora, bi.Folio_Incidencia, bi.Fecha_cambio, bi.Accion,bi.Descripcion AS 'Descripcion Bitacora',Estado_anterior,Estado_nuevo,CONCAT(E3.Nombre,' ',E3.ApellidoPat,' ', E3.ApellidoMat) AS 'Responsable Bitacora',i.Descripcion as 'Descripcion Incidencia', i.Fecha as 'Fecha Incidencia', i.Prioridad, i.Hora, ed.Nombre as 'Edificio', a.Nombre as 'Aula',ta.Nombre AS 'Tipo Aula', CONCAT(E.Nombre, ' ', E.ApellidoPat, ' ', E.ApellidoMat) AS 'Emisor', CONCAT(E2.Nombre, ' ', E2.ApellidoPat, ' ', E2.ApellidoMat) AS 'Receptor', eq.Version  FROM bitacoraincidencias bi
+        const [rows] = await connection.query(`SELECT bi.Id_Bitacora, bi.Folio_Incidencia, bi.Fecha_cambio,bi.Descripcion AS 'Descripcion Bitacora',i.Descripcion as 'Descripcion Incidencia', i.Fecha as 'Fecha Incidencia',bi.Estado, i.Prioridad, i.Hora, ed.Nombre as 'Edificio', a.Nombre as 'Aula',ta.Nombre AS 'Tipo Aula', CONCAT(E.Nombre, ' ', E.ApellidoPat, ' ', E.ApellidoMat) AS 'Emisor', CONCAT(E2.Nombre, ' ', E2.ApellidoPat, ' ', E2.ApellidoMat) AS 'Receptor', eq.Version  FROM bitacoraincidencias bi
                     INNER JOIN incidencia i ON bi.Folio_Incidencia = i.Folio
                     INNER JOIN aula a ON i.ID_Aula = a.ID_Aul
                     INNER JOIN edificio ed ON a.ID_Edif = ed.ID_Edif
                     INNER JOIN empleado e ON i.ID_Emi = e.ID_Emp
                     INNER JOIN empleado e2 ON e2.ID_Emp = i.ID_Rec
-                    INNER JOIN empleado e3 ON e3.ID_Emp = bi.ID_Emp
                     INNER JOIN equipo eq ON eq.ID_Aul = a.ID_Aul
                     INNER JOIN tipoaula ta ON ta.ID_TipoAula = a.ID_TipoAula;
                 `);
@@ -34,15 +33,14 @@ export const getLogsDetails = async () => {
 //obtener una bitacora por id
 export const getLogById = async (id: number) => {
     try {
-        const [rows] = await connection.query(`SELECT bi.Id_Bitacora, bi.Folio_Incidencia, bi.Fecha_cambio, bi.Accion,bi.Descripcion AS 'Descripcion Bitacora',Estado_anterior,Estado_nuevo,CONCAT(E3.Nombre,' ',E3.ApellidoPat,' ', E3.ApellidoMat) AS 'Responsable Bitacora',i.Descripcion as 'Descripcion Incidencia', i.Fecha as 'Fecha Incidencia', i.Prioridad, i.Hora, ed.Nombre as 'Edificio', a.Nombre as 'Aula',ta.Nombre AS 'Tipo Aula', CONCAT(E.Nombre, ' ', E.ApellidoPat, ' ', E.ApellidoMat) AS 'Emisor', CONCAT(E2.Nombre, ' ', E2.ApellidoPat, ' ', E2.ApellidoMat) AS 'Receptor', eq.Version  FROM bitacoraincidencias bi
+        const [rows] = await connection.query(`SELECT bi.Id_Bitacora, bi.Folio_Incidencia, bi.Fecha_cambio,bi.Descripcion AS 'Descripcion Bitacora',i.Descripcion as 'Descripcion Incidencia', i.Fecha as 'Fecha Incidencia',bi.Estado, i.Prioridad, i.Hora, ed.Nombre as 'Edificio', a.Nombre as 'Aula',ta.Nombre AS 'Tipo Aula', CONCAT(E.Nombre, ' ', E.ApellidoPat, ' ', E.ApellidoMat) AS 'Emisor', CONCAT(E2.Nombre, ' ', E2.ApellidoPat, ' ', E2.ApellidoMat) AS 'Receptor', eq.Version  FROM bitacoraincidencias bi
                     INNER JOIN incidencia i ON bi.Folio_Incidencia = i.Folio
                     INNER JOIN aula a ON i.ID_Aula = a.ID_Aul
                     INNER JOIN edificio ed ON a.ID_Edif = ed.ID_Edif
                     INNER JOIN empleado e ON i.ID_Emi = e.ID_Emp
                     INNER JOIN empleado e2 ON e2.ID_Emp = i.ID_Rec
-                    INNER JOIN empleado e3 ON e3.ID_Emp = bi.ID_Emp
                     INNER JOIN equipo eq ON eq.ID_Aul = a.ID_Aul
-                    INNER JOIN tipoaula ta ON ta.ID_TipoAula = a.ID_TipoAula WHERE bi.ID_Bitacora = ?;`, 
+                    INNER JOIN tipoaula ta ON ta.ID_TipoAula = a.ID_TipoAula WHERE bi.Id_Bitacora = ?;`, 
                     [id]);
         return rows[0];
     } catch (error) {
@@ -53,7 +51,7 @@ export const getLogById = async (id: number) => {
 //agregar una bitacora
 export const addLog = async (log: LogNew) => {
     try {
-        const [rows] = await connection.query('INSERT INTO bitacoraincidencias (Folio_Incidencia, Fecha_Cambio, Accion, Descripcion, Estado_anterior, Estado_nuevo,ID_Emp) values (?,?,?,?,?,?,?)', [log.Folio_Incidencia, log.Fecha_Cambio, log.Accion, log.Descripcion, log.Estado_anterior, log.Estado_nuevo, log.ID_Emp]);
+        const [rows] = await connection.query('INSERT INTO bitacoraincidencias (Folio_Incidencia, Fecha_Cambio, Descripcion, ID_Emp) values (?,?,?,?)', [log.Folio_Incidencia, log.Fecha_Cambio,  log.Descripcion, log.ID_Emp]);
         return rows;
     } catch (error) {
         return { error: "no se pudo agregar la bitacora" };
